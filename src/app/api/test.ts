8f067a3a2e5b0b023e7dd9522ef8bd7fca3af310
김동현 <rlaehdgus3543@naver.com>
2023-07-10 22:57:23 +0900

    next server file 추가

---
+++ src/app/api/test.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method Not Allowed' })
+    return
+  }
+  res.status(200).json({ message: '요청이 성공적으로 수행되었습니다.' })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+import { useState } from 'react'
 import styles from './page.module.css'
+import axios from 'axios'
 
 export default function Home() {
+  const apiTest = async () => {
+    const response = await axios({
+      method: 'POST',
+      url: '/api/test',
+
+      data: '123',
+    })
+    console.log('들어옴', response.data)
+  }
   return (
     <main className={styles.main}>
-      <div>server test</div>
+      <div>
+        <div>server test</div>
+        <br />
+        <button onClick={apiTest}>click!</button>
+      </div>
     </main>
   )
 }
